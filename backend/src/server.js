f996d3f330d470def20b88b6a5048bdd8bf67655
Paloma Monteiro <paloma.l.monteiro@gmail.com>
2019-10-03 20:03:04 -0300

    projeto inicial com back e front em desenvolvimento

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://root:root@cluster0-i1sny.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
+
+//req.query => get (para filtros)
+//req.params => delete,put ( edição e delete)
+//req.body => acessar corpo requisição (create, edição)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
